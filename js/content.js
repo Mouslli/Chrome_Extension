c141fabae6b90e63339ce9571fce5c87b07e05af
Mouslli <abdullah.mouslli@gmail.com>
2021-02-25 16:01:15 -0700

    fix audio, overlay & add word replace

--- js/content.js
+++ js/content.js
@@ -1,3 +1,5 @@
+
+// add overlay  
 chrome.runtime.onMessage.addListener(
 function(request)	{
 var	initiate	=	request.message;
@@ -5,4 +7,140 @@ console.log("clicked")
 	$('body').toggleClass("Overlay");
 	}
 );
+
+// // remove overlay 
+// chrome.runtime.onMessage.addListener(
+// function(request)	{
+// var	pausing	=	request.message;
+// console.log("clicked")
+// 	$('body').removeClass("Overlay");
+// 	}
+// );
+
+
+// replace words
+// code from 9To5Google: https://9to5google.com/2015/06/14/how-to-make-a-chrome-extensions/
+
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.
+            replace(/Trump/gi, 'Baby Fingers');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
+
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.
+            replace(/COVID-19, COVID/gi, 'cute little virus');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
+
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.
+            replace(/oil/gi, 'Earth Juice');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
+
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.
+            replace(/Election/gi, 'popularity contest');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
+
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.
+            replace(/Police/gi, 'PoPo');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
+
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.
+            replace(/Riot/gi, 'Bongo party');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
+
+
+
 
--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,4 @@
+// send a message when the start button is clicked
 $('#start').on('click',	Begin);
 function Begin	()	{
 	console.log ("clicking")
@@ -7,14 +8,29 @@ function Begin	()	{
 	 });
 }
 
+// send a message when the pause button is clicked
+$('#pause').on('click', stop);
+function stop	()	{
+	console.log ("clicking")
+	var	pausing	=	$(this).attr('id');
+		 chrome.tabs.query({active:	true,	currentWindow:	true},	function(tabs)	{
+	 		chrome.tabs.sendMessage(tabs[0].id,	{message:	pausing});
+	 });
+}
+
+// add audio file 
 var myAudio = new Audio(chrome.runtime.getURL("sounds/meditation.mp3"));
 
+// when start button clicked start playing audio
 $('#start').on ("click", function (){
 	myAudio.play();
 
 });
 
-
+// when clicked pause audio
 $('#pause').on("click", function(){
 	myAudio.pause();
 })
+
+
+
